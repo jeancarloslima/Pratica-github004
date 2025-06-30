7c7b2869b79af44cd49ec6cdab3d0e663c1519f8
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-06-30 13:08:08 -0300

    Adicionando teste 6

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Teste 2");
 console.log("Teste 3");
 console.log("Teste 4");
 console.log("Teste 5");
+console.log("Teste 6");
 console.log("Outro teste 1");
 console.log("Outro teste 2");
 console.log("Outro teste 3");
