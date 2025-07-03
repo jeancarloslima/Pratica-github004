61c88c27281d7ed340750a8b96b9cec9969ccca0
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-07-03 12:34:34 -0300

    Adicionando teste 10

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ console.log("Teste 6");
 console.log("Teste 7");
 console.log("Teste 8");
 console.log("Teste 9");
+console.log("Teste 10");
 console.log("Outro teste 1");
 console.log("Outro teste 2");
 console.log("Outro teste 3");
