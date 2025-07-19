8d261a5cc93ca1f347e4b8379808d366e68c629b
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-07-19 10:58:08 -0300

    Adicionando outro teste 14

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ console.log("Outro teste 10");
 console.log("Outro teste 11");
 console.log("Outro teste 12");
 console.log("Outro teste 13");
+console.log("Outro teste 14");
 console.log("Código aí");
 console.log("Código aí 2");
 console.log("Código aí 3");
